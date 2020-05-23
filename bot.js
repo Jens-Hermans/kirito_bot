85aaec37b9fc325e9caf854f62bf4fda5e53a820
Jens Hermans <r0739255@thomasmore.be>
2020-05-23 22:52:19 +0200

    test

--- bot.js
+++ bot.js
@@ -1,69 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setStatus('available')
-    client.user.setPresence({
-        game: {
-            name: 'With Asada',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/xealiusrl"
-        }
-    });
 });
+
 client.on('message', message => {
-    if (message.content === '/q')
-    {
-        var i = Math.floor(Math.random() * 4) + 1;
-        var randomImg = "Images/sinon" + i + ".jpg"
-        message.channel.send({files: [randomImg]});
+    if (message.content === 'ping') {
+        message.reply('pong');
     }
-    else if (message.content === '/help')
-    {
-        message.channel.send("```diff\n-Help```");
-    }
-    else if (message.content.startsWith ('/embed'))
-    {
-        const embed = new Discord.RichEmbed();
-        var mention = message.mentions.users.first();
-        if (mention == null){
-            embed.setTitle("Your Avatar");
-            embed.setThumbnail(message.author.displayAvatarURL);
-            embed.setColor("37bceb");
-            message.channel.send({embed});
-        }
-        else{
-            embed.setTitle(`${mention.username}'s avatar!`);
-            embed.setThumbnail(mention.displayAvatarURL);
-            embed.setColor("f7d456");
-            message.channel.send({embed});
-        }
-    }
-    else if (message.content === `/server`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    }
-    else if (message.content.startsWith ('/u')){
-        var mention = message.mentions.users.first();
-        if (mention == null){
-            message.channel.send('pls tag someone');
-            return;
-        }
-        message.channel.send(`Hello ${mention} :D`);
-    }
-    else if (message.content.startsWith ('/send')) {
-        var mention = message.mentions.users.first();
-        if (mention == null){
-            message.channel.send('pls tag someone');
-            return;
-        }
-        message.delete();
-        mentionMessage = message.content.slice(6);
-        mention.sendMessage (mentionMessage);
-    }
-    else if (message.content === '/join'){
-
-    }
-
 });
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
