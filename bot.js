165280d6d06d3ab01b3b6e36abdcf2ef0b1ae4e4
Jens Hermans <r0739255@thomasmore.be>
2020-05-23 22:49:40 +0200

    test

--- bot.js
+++ bot.js
@@ -1,129 +1,69 @@
-const Discord = require("discord.js");
-const { prefix, token } = require("./config.json");
-const ytdl = require("ytdl-core");
-
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
-const queue = new Map();
-
-client.once("ready", () => {
-    console.log("Ready!");
-});
-
-client.once("reconnecting", () => {
-    console.log("Reconnecting!");
-});
-
-client.once("disconnect", () => {
-    console.log("Disconnect!");
-});
-
-client.on("message", async message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    const serverQueue = queue.get(message.guild.id);
-
-    if (message.content.startsWith(`${prefix}play`)) {
-        execute(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${prefix}skip`)) {
-        skip(message, serverQueue);
-        return;
-    } else if (message.content.startsWith(`${prefix}stop`)) {
-        stop(message, serverQueue);
-        return;
-    } else {
-        message.channel.send("You need to enter a valid command!");
-    }
-});
-
-async function execute(message, serverQueue) {
-    const args = message.content.split(" ");
-
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-        return message.channel.send(
-            "You need to be in a voice channel to play music!"
-        );
-    const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-        return message.channel.send(
-            "I need the permissions to join and speak in your voice channel!"
-        );
-    }
-
-    const songInfo = await ytdl.getInfo(args[1]);
-    const song = {
-        title: songInfo.title,
-        url: songInfo.video_url
-    };
-
-    if (!serverQueue) {
-        const queueContruct = {
-            textChannel: message.channel,
-            voiceChannel: voiceChannel,
-            connection: null,
-            songs: [],
-            volume: 5,
-            playing: true
-        };
-
-        queue.set(message.guild.id, queueContruct);
-
-        queueContruct.songs.push(song);
-
-        try {
-            var connection = await voiceChannel.join();
-            queueContruct.connection = connection;
-            play(message.guild, queueContruct.songs[0]);
-        } catch (err) {
-            console.log(err);
-            queue.delete(message.guild.id);
-            return message.channel.send(err);
+client.on('ready', () => {
+    console.log('I am ready!');
+    client.user.setStatus('available')
+    client.user.setPresence({
+        game: {
+            name: 'With Asada',
+            type: "STREAMING",
+            url: "https://www.twitch.tv/xealiusrl"
         }
-    } else {
-        serverQueue.songs.push(song);
-        return message.channel.send(`${song.title} has been added to the queue!`);
+    });
+});
+client.on('message', message => {
+    if (message.content === '/q')
+    {
+        var i = Math.floor(Math.random() * 4) + 1;
+        var randomImg = "Images/sinon" + i + ".jpg"
+        message.channel.send({files: [randomImg]});
     }
-}
+    else if (message.content === '/help')
+    {
+        message.channel.send("```diff\n-Help```");
+    }
+    else if (message.content.startsWith ('/embed'))
+    {
+        const embed = new Discord.RichEmbed();
+        var mention = message.mentions.users.first();
+        if (mention == null){
+            embed.setTitle("Your Avatar");
+            embed.setThumbnail(message.author.displayAvatarURL);
+            embed.setColor("37bceb");
+            message.channel.send({embed});
+        }
+        else{
+            embed.setTitle(`${mention.username}'s avatar!`);
+            embed.setThumbnail(mention.displayAvatarURL);
+            embed.setColor("f7d456");
+            message.channel.send({embed});
+        }
+    }
+    else if (message.content === `/server`) {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    }
+    else if (message.content.startsWith ('/u')){
+        var mention = message.mentions.users.first();
+        if (mention == null){
+            message.channel.send('pls tag someone');
+            return;
+        }
+        message.channel.send(`Hello ${mention} :D`);
+    }
+    else if (message.content.startsWith ('/send')) {
+        var mention = message.mentions.users.first();
+        if (mention == null){
+            message.channel.send('pls tag someone');
+            return;
+        }
+        message.delete();
+        mentionMessage = message.content.slice(6);
+        mention.sendMessage (mentionMessage);
+    }
+    else if (message.content === '/join'){
 
-function skip(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    if (!serverQueue)
-        return message.channel.send("There is no song that I could skip!");
-    serverQueue.connection.dispatcher.end();
-}
-
-function stop(message, serverQueue) {
-    if (!message.member.voice.channel)
-        return message.channel.send(
-            "You have to be in a voice channel to stop the music!"
-        );
-    serverQueue.songs = [];
-    serverQueue.connection.dispatcher.end();
-}
-
-function play(guild, song) {
-    const serverQueue = queue.get(guild.id);
-    if (!song) {
-        serverQueue.voiceChannel.leave();
-        queue.delete(guild.id);
-        return;
     }
 
-    const dispatcher = serverQueue.connection
-        .play(ytdl(song.url))
-        .on("finish", () => {
-            serverQueue.songs.shift();
-            play(guild, serverQueue.songs[0]);
-        })
-        .on("error", error => console.error(error));
-    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-    serverQueue.textChannel.send(`Start playing: **${song.title}**`);
-}
-
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+});
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
